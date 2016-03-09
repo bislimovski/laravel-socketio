d75ad23c944565616f184a3cf9bd893e71676746
Ivan <ivan.bislimovski@gmail.com>
2016-03-09 21:05:26 +0100

    laravel

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -43,11 +43,5 @@
             }
         });
         </script>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
-        </div>
     </body>
 </html>
